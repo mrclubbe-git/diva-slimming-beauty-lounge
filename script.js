1d206d59e4ba70196e107033ceecf672352bca93
mrclubbe-git <mrclubbe@gmail.com>
2026-06-15 10:46:38 +0200

    Remove unused logo fallback script

--- script.js
+++ script.js
@@ -14,17 +14,3 @@ if (header && toggle) {
     });
   });
 }
-
-document.querySelectorAll("img[data-fallback-src]").forEach((image) => {
-  image.addEventListener(
-    "error",
-    () => {
-      const fallbackSrc = image.dataset.fallbackSrc;
-
-      if (fallbackSrc && !image.src.endsWith(fallbackSrc)) {
-        image.src = fallbackSrc;
-      }
-    },
-    { once: true }
-  );
-});
